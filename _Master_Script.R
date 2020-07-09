c6a40a2d8c333c030efc3067cecc74f73ceb2c9c
Ubuntu <ubuntu@ip-172-31-27-71.eu-west-1.compute.internal>
2020-07-09 19:21:16 +0000

    YAY

--- _Master_Script.R
+++ _Master_Script.R
@@ -5,10 +5,10 @@
 source('./Scripts/virus_data.R')
 
 # Collect static variables
-source('./Scripts/Gather_GHSindex_data_static.R', encoding="utf-8")
+source('./Scripts/Gather_GHSindex_data_static.R', encoding="WINDOWS-1252")
 
 # Clean the data for implementation in the ML model
-source('./Scripts/data_cleaning_oxford.R', encoding="utf-8")
+source('./Scripts/data_cleaning_oxford.R')
 
 # # Make United States GIF plots
 # source('./Scripts/United_States_Animation.R')
@@ -17,9 +17,9 @@ source('./Scripts/data_cleaning_oxford.R', encoding="utf-8")
 # source('./Scripts/MountainRangeAnimationGIFs.R')
 
 # Variable Selection Using VSURF
-source('./Scripts/run_VSURF_and_WT_oxford.R', encoding="utf-8")
+source('./Scripts/run_VSURF_and_WT_oxford.R')
 
-source('./Scripts/save_LOO_models.R', encoding="utf-8")
+source('./Scripts/save_LOO_models.R')
 
 # Run a specific scenario
 # source('./Scripts/Analysis_2_Model_R0_5_14_WT_SEIR_ribbon.R')
